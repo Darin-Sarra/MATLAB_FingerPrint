3d61a6badbbba3ac9e6b7a6622406b10bc57f047
Darin-Sarra <44895745+Darin-Sarra@users.noreply.github.com>
2018-12-27 18:26:27 +0100

    Add files via upload

--- FingerPrint/sourcecode.m
+++ FingerPrint/sourcecode.m
@@ -1,4 +1,10 @@
-% Feel Free To Visit Us :
-% http://matlab-recognition-code.com/matlab-fingerprint-recognition-system-full-source-code/
-%For further question please email US darin_sarri@matlab-recognition-code.com
-
+% -----Project : Fingerprint Recognition Project-----
+%----------------------------------------------------
+% Please Donate to obtain the Full source code.
+% Link : https://matlab-recognition-code.com/matlab-fingerprint-recognition-system-full-source-code/
+% 
+% Developer: PhD, Hamdi Boukamcha, Faculty of Science of Monastir, Tunisia.
+% Email: Contact@matlab-recognition-code.com
+% WhatsAPP: +21650674269
+% Website: https://Matlab-Recognition-Code.com
+%
